d4d78198d37c9600295f7d989847d20e2370b61c
guruviswa <96130263+guruviswa@users.noreply.github.com>
2023-11-10 20:18:41 +0530

    My first mini project using html,css,javascript

---
+++ project.js
@@ -0,0 +1,16 @@
+function show(){
+ 
+document.getElementById("p1").innerHTML=" Good morning sir/mam,first of all thanks for the oppourtunity to introduce myself myself "+
+n1.value+" i am coming from "+a1.value+" i completed my schooling in "+e1.value+" and i completed my graduation in "+
+c1.value+" with"+cg1.value+" CGPA"+" my short term goal is "+s1.value+" and my long term goal is "+l1.value+
+" coming to my skills i have a good knowledge in "+ sk1.value+" apart from my skill i strongly believe that good communication"+ 
+" and team work is very important in every perfession and i strongly believe that i have that skils if you gave me "+
+" oppourity i defenitly contribute my energy,skills to you team.so these are the few things about myself "
++" once again thanks for the oppourtinity to introduce myself" ;
+
+}
+
+// get placed in the reputed company
+// aheive position in the company which i placed
+// html,css,javascript,
+// react js,java,sql
